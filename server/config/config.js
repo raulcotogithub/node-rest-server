0afe5edecd64ab486e869410eef58ed3a1ea85da
Raul <raul.coto.diaz@gmail.com>
2020-10-04 00:26:35 -0600

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,19 @@
 //puerto
 //=======================================
 process.env.PORT = process.env.PORT || 3000;
+
+//Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//=======================================
+// Base de Datos
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://raulcoto:xgTAiJpZ9caMbu42@cluster0.w0evj.mongodb.net/cafe';
+};
+
+
+process.env.URLDB = urlDB;
