389339c8a0a4362d630fc2cbafdacd9c225407fa
Raul <raul.coto.diaz@gmail.com>
2020-10-04 19:26:08 -0600

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -14,7 +14,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://raulcoto:xgTAiJpZ9caMbu42@cluster0.w0evj.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 };
 
 
